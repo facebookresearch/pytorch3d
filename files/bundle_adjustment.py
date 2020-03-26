23fddf074c6234135027e9a7bd04d027cf961e61
Nikhila Ravi <nikhilar@fb.com>
2020-03-26 11:13:34 -0700

    Update latest version of site

---
+++ files/bundle_adjustment.py
@@ -0,0 +1,251 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+
+
+# # Absolute camera orientation given set of relative camera pairs
+# 
+# This tutorial showcases the `cameras`, `transforms` and `so3` API.
+# 
+# The problem we deal with is defined as follows:
+# 
+# Given an optical system of $N$ cameras with extrinsics $\{g_1, ..., g_N | g_i \in SE(3)\}$, and a set of relative camera positions $\{g_{ij} | g_{ij}\in SE(3)\}$ that map between coordinate frames of randomly selected pairs of cameras $(i, j)$, we search for the absolute extrinsic parameters $\{g_1, ..., g_N\}$ that are consistent with the relative camera motions.
+# 
+# More formally:
+# $$
+# g_1, ..., g_N = 
+# {\arg \min}_{g_1, ..., g_N} \sum_{g_{ij}} d(g_{ij}, g_i^{-1} g_j),
+# $$,
+# where $d(g_i, g_j)$ is a suitable metric that compares the extrinsics of cameras $g_i$ and $g_j$. 
+# 
+# Visually, the problem can be described as follows. The picture below depicts the situation at the beginning of our optimization. The ground truth cameras are plotted in purple while the randomly initialized estimated cameras are plotted in orange:
+# ![Initialization](https://github.com/facebookresearch/pytorch3d/blob/master/docs/tutorials/data/bundle_adjustment_initialization.png?raw=1)
+# 
+# Our optimization seeks to align the estimated (orange) cameras with the ground truth (purple) cameras, by minimizing the discrepancies between pairs of relative cameras. Thus, the solution to the problem should look as follows:
+# ![Solution](https://github.com/facebookresearch/pytorch3d/blob/master/docs/tutorials/data/bundle_adjustment_final.png?raw=1)
+# 
+# In practice, the camera extrinsics $g_{ij}$ and $g_i$ are represented using objects from the `SfMPerspectiveCameras` class initialized with the corresponding rotation and translation matrices `R_absolute` and `T_absolute` that define the extrinsic parameters $g = (R, T); R \in SO(3); T \in \mathbb{R}^3$. In order to ensure that `R_absolute` is a valid rotation matrix, we represent it using an exponential map (implemented with `so3_exponential_map`) of the axis-angle representation of the rotation `log_R_absolute`.
+# 
+# Note that the solution to this problem could only be recovered up to an unknown global rigid transformation $g_{glob} \in SE(3)$. Thus, for simplicity, we assume knowledge of the absolute extrinsics of the first camera $g_0$. We set $g_0$ as a trivial camera $g_0 = (I, \vec{0})$.
+# 
+
+# ## 0. Install and Import Modules
+
+# If `torch`, `torchvision` and `pytorch3d` are not installed, run the following cell:
+
+# In[1]:
+
+
+get_ipython().system('pip install torch torchvision')
+get_ipython().system("pip install 'git+https://github.com/facebookresearch/pytorch3d.git@stable'")
+
+
+# In[3]:
+
+
+# imports
+import torch
+from pytorch3d.transforms.so3 import (
+    so3_exponential_map,
+    so3_relative_angle,
+)
+from pytorch3d.renderer.cameras import (
+    SfMPerspectiveCameras,
+)
+
+# add path for demo utils
+import sys
+import os
+sys.path.append(os.path.abspath(''))
+
+# set for reproducibility
+torch.manual_seed(42)
+
+
+# If using **Google Colab**, fetch the utils file for plotting the camera scene, and the ground truth camera positions:
+
+# In[2]:
+
+
+get_ipython().system('wget https://raw.githubusercontent.com/facebookresearch/pytorch3d/master/docs/tutorials/utils/camera_visualization.py')
+from camera_visualization import plot_camera_scene
+
+get_ipython().system('mkdir data')
+get_ipython().system('wget -P data https://raw.githubusercontent.com/facebookresearch/pytorch3d/master/docs/tutorials/data/camera_graph.pth')
+
+
+# OR if running **locally** uncomment and run the following cell:
+
+# In[ ]:
+
+
+# from utils import plot_camera_scene
+
+
+# ## 1. Set up Cameras and load ground truth positions
+
+# In[ ]:
+
+
+# load the SE3 graph of relative/absolute camera positions
+camera_graph_file = './data/camera_graph.pth'
+(R_absolute_gt, T_absolute_gt),     (R_relative, T_relative),     relative_edges =         torch.load(camera_graph_file)
+
+# create the relative cameras
+cameras_relative = SfMPerspectiveCameras(
+    R = R_relative.cuda(),
+    T = T_relative.cuda(),
+    device = "cuda",
+)
+
+# create the absolute ground truth cameras
+cameras_absolute_gt = SfMPerspectiveCameras(
+    R = R_absolute_gt.cuda(),
+    T = T_absolute_gt.cuda(),
+    device = "cuda",
+)
+
+# the number of absolute camera positions
+N = R_absolute_gt.shape[0]
+
+
+# ## 2. Define optimization functions
+# 
+# ### Relative cameras and camera distance
+# We now define two functions crucial for the optimization.
+# 
+# **`calc_camera_distance`** compares a pair of cameras. This function is important as it defines the loss that we are minimizing. The method utilizes the `so3_relative_angle` function from the SO3 API.
+# 
+# **`get_relative_camera`** computes the parameters of a relative camera that maps between a pair of absolute cameras. Here we utilize the `compose` and `inverse` class methods from the PyTorch3D Transforms API.
+
+# In[ ]:
+
+
+def calc_camera_distance(cam_1, cam_2):
+    """
+    Calculates the divergence of a batch of pairs of cameras cam_1, cam_2.
+    The distance is composed of the cosine of the relative angle between 
+    the rotation components of the camera extrinsics and the l2 distance
+    between the translation vectors.
+    """
+    # rotation distance
+    R_distance = (1.-so3_relative_angle(cam_1.R, cam_2.R, cos_angle=True)).mean()
+    # translation distance
+    T_distance = ((cam_1.T - cam_2.T)**2).sum(1).mean()
+    # the final distance is the sum
+    return R_distance + T_distance
+
+def get_relative_camera(cams, edges):
+    """
+    For each pair of indices (i,j) in "edges" generate a camera
+    that maps from the coordinates of the camera cams[i] to 
+    the coordinates of the camera cams[j]
+    """
+
+    # first generate the world-to-view Transform3d objects of each 
+    # camera pair (i, j) according to the edges argument
+    trans_i, trans_j = [
+        SfMPerspectiveCameras(
+            R = cams.R[edges[:, i]],
+            T = cams.T[edges[:, i]],
+            device = "cuda",
+        ).get_world_to_view_transform()
+         for i in (0, 1)
+    ]
+    
+    # compose the relative transformation as g_i^{-1} g_j
+    trans_rel = trans_i.inverse().compose(trans_j)
+    
+    # generate a camera from the relative transform
+    matrix_rel = trans_rel.get_matrix()
+    cams_relative = SfMPerspectiveCameras(
+                        R = matrix_rel[:, :3, :3],
+                        T = matrix_rel[:, 3, :3],
+                        device = "cuda",
+                    )
+    return cams_relative
+
+
+# ## 3. Optimization
+# Finally, we start the optimization of the absolute cameras.
+# 
+# We use SGD with momentum and optimize over `log_R_absolute` and `T_absolute`. 
+# 
+# As mentioned earlier, `log_R_absolute` is the axis angle representation of the rotation part of our absolute cameras. We can obtain the 3x3 rotation matrix `R_absolute` that corresponds to `log_R_absolute` with:
+# 
+# `R_absolute = so3_exponential_map(log_R_absolute)`
+# 
+
+# In[8]:
+
+
+# initialize the absolute log-rotations/translations with random entries
+log_R_absolute_init = torch.randn(N, 3).float().cuda()
+T_absolute_init = torch.randn(N, 3).float().cuda()
+
+# furthermore, we know that the first camera is a trivial one 
+#    (see the description above)
+log_R_absolute_init[0, :] = 0.
+T_absolute_init[0, :] = 0.
+
+# instantiate a copy of the initialization of log_R / T
+log_R_absolute = log_R_absolute_init.clone().detach()
+log_R_absolute.requires_grad = True
+T_absolute = T_absolute_init.clone().detach()
+T_absolute.requires_grad = True
+
+# the mask the specifies which cameras are going to be optimized
+#     (since we know the first camera is already correct, 
+#      we only optimize over the 2nd-to-last cameras)
+camera_mask = torch.ones(N, 1).float().cuda()
+camera_mask[0] = 0.
+
+# init the optimizer
+optimizer = torch.optim.SGD([log_R_absolute, T_absolute], lr=.1, momentum=0.9)
+
+# run the optimization
+n_iter = 2000  # fix the number of iterations
+for it in range(n_iter):
+    # re-init the optimizer gradients
+    optimizer.zero_grad()
+
+    # compute the absolute camera rotations as 
+    # an exponential map of the logarithms (=axis-angles)
+    # of the absolute rotations
+    R_absolute = so3_exponential_map(log_R_absolute * camera_mask)
+
+    # get the current absolute cameras
+    cameras_absolute = SfMPerspectiveCameras(
+        R = R_absolute,
+        T = T_absolute * camera_mask,
+        device = "cuda",
+    )
+
+    # compute the relative cameras as a compositon of the absolute cameras
+    cameras_relative_composed =         get_relative_camera(cameras_absolute, relative_edges)
+
+    # compare the composed cameras with the ground truth relative cameras
+    # camera_distance corresponds to $d$ from the description
+    camera_distance =         calc_camera_distance(cameras_relative_composed, cameras_relative)
+
+    # our loss function is the camera_distance
+    camera_distance.backward()
+    
+    # apply the gradients
+    optimizer.step()
+
+    # plot and print status message
+    if it % 200==0 or it==n_iter-1:
+        status = 'iteration=%3d; camera_distance=%1.3e' % (it, camera_distance)
+        plot_camera_scene(cameras_absolute, cameras_absolute_gt, status)
+
+print('Optimization finished.')
+
+
+# ## 4. Conclusion 
+# 
+# In this tutorial we learnt how to initialize a batch of SfM Cameras, set up loss functions for bundle adjustment, and run an optimization loop. 
---
+++ files/render_textured_meshes.py
@@ -0,0 +1,299 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+
+
+# # Render a textured mesh
+# 
+# This tutorial shows how to:
+# - load a mesh and textures from an `.obj` file. 
+# - set up a renderer 
+# - render the mesh 
+# - vary the rendering settings such as lighting and camera position
+# - use the batching features of the pytorch3d API to render the mesh from different viewpoints
+
+# ## 0. Install and Import modules
+
+# If `torch`, `torchvision` and `pytorch3d` are not installed, run the following cell:
+
+# In[1]:
+
+
+get_ipython().system('pip install torch torchvision')
+get_ipython().system("pip install 'git+https://github.com/facebookresearch/pytorch3d.git@stable'")
+
+
+# In[1]:
+
+
+import os
+import torch
+import matplotlib.pyplot as plt
+from skimage.io import imread
+
+# Util function for loading meshes
+from pytorch3d.io import load_objs_as_meshes
+
+# Data structures and functions for rendering
+from pytorch3d.structures import Meshes, Textures
+from pytorch3d.renderer import (
+    look_at_view_transform,
+    OpenGLPerspectiveCameras, 
+    PointLights, 
+    DirectionalLights, 
+    Materials, 
+    RasterizationSettings, 
+    MeshRenderer, 
+    MeshRasterizer,  
+    TexturedSoftPhongShader
+)
+
+# add path for demo utils functions 
+import sys
+import os
+sys.path.append(os.path.abspath(''))
+
+
+# If using **Google Colab**, fetch the utils file for plotting image grids:
+
+# In[2]:
+
+
+get_ipython().system('wget https://raw.githubusercontent.com/facebookresearch/pytorch3d/master/docs/tutorials/utils/plot_image_grid.py')
+from plot_image_grid import image_grid
+
+
+# OR if running **locally** uncomment and run the following cell:
+
+# In[13]:
+
+
+# from utils import image_grid
+
+
+# ### 1. Load a mesh and texture file
+# 
+# Load an `.obj` file and it's associated `.mtl` file and create a **Textures** and **Meshes** object. 
+# 
+# **Meshes** is a unique datastructure provided in PyTorch3D for working with batches of meshes of different sizes. 
+# 
+# **Textures** is an auxillary datastructure for storing texture information about meshes. 
+# 
+# **Meshes** has several class methods which are used throughout the rendering pipeline.
+
+# If running this notebook using **Google Colab**, run the following cell to fetch the mesh obj and texture files and save it at the path `data/cow_mesh`:
+# If running locally, the data is already available at the correct path. 
+
+# In[3]:
+
+
+get_ipython().system('mkdir -p data/cow_mesh')
+get_ipython().system('wget -P data/cow_mesh https://dl.fbaipublicfiles.com/pytorch3d/data/cow_mesh/cow.obj')
+get_ipython().system('wget -P data/cow_mesh https://dl.fbaipublicfiles.com/pytorch3d/data/cow_mesh/cow.mtl')
+get_ipython().system('wget -P data/cow_mesh https://dl.fbaipublicfiles.com/pytorch3d/data/cow_mesh/cow_texture.png')
+
+
+# In[2]:
+
+
+# Setup
+device = torch.device("cuda:0")
+torch.cuda.set_device(device)
+
+# Set paths
+DATA_DIR = "./data"
+obj_filename = os.path.join(DATA_DIR, "cow_mesh/cow.obj")
+
+# Load obj file
+mesh = load_objs_as_meshes([obj_filename], device=device)
+texture_image=mesh.textures.maps_padded()
+
+
+# #### Let's visualize the texture map
+
+# In[3]:
+
+
+plt.figure(figsize=(7,7))
+plt.imshow(texture_image.squeeze().cpu().numpy())
+plt.grid("off");
+plt.axis('off');
+
+
+# ## 2. Create a renderer
+# 
+# A renderer in PyTorch3D is composed of a **rasterizer** and a **shader** which each have a number of subcomponents such as a **camera** (orthographic/perspective). Here we initialize some of these components and use default values for the rest.
+# 
+# In this example we will first create a **renderer** which uses a **perspective camera**, a **point light** and applies **phong shading**. Then we learn how to vary different components using the modular API.  
+
+# In[4]:
+
+
+# Initialize an OpenGL perspective camera.
+# With world coordinates +Y up, +X left and +Z in, the front of the cow is facing the -Z direction. 
+# So we move the camera by 180 in the azimuth direction so it is facing the front of the cow. 
+R, T = look_at_view_transform(2.7, 0, 180) 
+cameras = OpenGLPerspectiveCameras(device=device, R=R, T=T)
+
+# Define the settings for rasterization and shading. Here we set the output image to be of size
+# 512x512. As we are rendering images for visualization purposes only we will set faces_per_pixel=1
+# and blur_radius=0.0. We also set bin_size and max_faces_per_bin to None which ensure that 
+# the faster coarse-to-fine rasterization method is used. Refer to rasterize_meshes.py for 
+# explanations of these parameters. Refer to docs/notes/renderer.md for an explanation of 
+# the difference between naive and coarse-to-fine rasterization. 
+raster_settings = RasterizationSettings(
+    image_size=512, 
+    blur_radius=0.0, 
+    faces_per_pixel=1, 
+    bin_size = None,  # this setting controls whether naive or coarse-to-fine rasterization is used
+    max_faces_per_bin = None  # this setting is for coarse rasterization
+)
+
+# Place a point light in front of the object. As mentioned above, the front of the cow is facing the 
+# -z direction. 
+lights = PointLights(device=device, location=[[0.0, 0.0, -3.0]])
+
+# Create a phong renderer by composing a rasterizer and a shader. The textured phong shader will 
+# interpolate the texture uv coordinates for each vertex, sample from a texture image and 
+# apply the Phong lighting model
+renderer = MeshRenderer(
+    rasterizer=MeshRasterizer(
+        cameras=cameras, 
+        raster_settings=raster_settings
+    ),
+    shader=TexturedSoftPhongShader(
+        device=device, 
+        cameras=cameras,
+        lights=lights
+    )
+)
+
+
+# ## 3. Render the mesh
+
+# The light is in front of the object so it is bright and the image has specular highlights.
+
+# In[5]:
+
+
+images = renderer(mesh)
+plt.figure(figsize=(10, 10))
+plt.imshow(images[0, ..., :3].cpu().numpy())
+plt.grid("off");
+plt.axis("off");
+
+
+# ## 4. Move the light behind the object and re-render
+# 
+# We can pass arbirary keyword arguments to the `rasterizer`/`shader` via the call to the `renderer` so the renderer does not need to be reinitialized if any of the settings change/
+# 
+# In this case, we can simply update the location of the lights and pass them into the call to the renderer. 
+# 
+# The image is now dark as there is only ambient lighting, and there are no specular highlights.
+
+# In[6]:
+
+
+# Now move the light so it is on the +Z axis which will be behind the cow. 
+lights.location = torch.tensor([0.0, 0.0, +1.0], device=device)[None]
+images = renderer(mesh, lights=lights)
+
+
+# In[7]:
+
+
+plt.figure(figsize=(10, 10))
+plt.imshow(images[0, ..., :3].cpu().numpy())
+plt.grid("off");
+plt.axis("off");
+
+
+# ## 5. Rotate the object, modify the material properties or light properties
+# 
+# We can also change many other settings in the rendering pipeline. Here we:
+# 
+# - change the **viewing angle** of the camera
+# - change the **position** of the point light
+# - change the **material reflectance** properties of the mesh
+
+# In[8]:
+
+
+# Rotate the object by increasing the elevation and azimuth angles
+R, T = look_at_view_transform(dist=2.7, elev=10, azim=-150)
+cameras = OpenGLPerspectiveCameras(device=device, R=R, T=T)
+
+# Move the light location so the light is shining on the cow's face.  
+lights.location = torch.tensor([[2.0, 2.0, -2.0]], device=device)
+
+# Change specular color to green and change material shininess 
+materials = Materials(
+    device=device,
+    specular_color=[[0.0, 1.0, 0.0]],
+    shininess=10.0
+)
+
+# Re render the mesh, passing in keyword arguments for the modified components.
+images = renderer(mesh, lights=lights, materials=materials, cameras=cameras)
+
+
+# In[9]:
+
+
+plt.figure(figsize=(10, 10))
+plt.imshow(images[0, ..., :3].cpu().numpy())
+plt.grid("off");
+plt.axis("off");
+
+
+# ## 6. Batched Rendering
+# 
+# One of the core design choices of the PyTorch3D API is to support **batched inputs for all components**. 
+# The renderer and associated components can take batched inputs and **render a batch of output images in one forward pass**. We will now use this feature to render the mesh from many different viewpoints.
+# 
+
+# In[10]:
+
+
+# Set batch size - this is the number of different viewpoints from which we want to render the mesh.
+batch_size = 20
+
+# Create a batch of meshes by repeating the cow mesh and associated textures. 
+# Meshes has a useful `extend` method which allows us do this very easily. 
+# This also extends the textures. 
+meshes = mesh.extend(batch_size)
+
+# Get a batch of viewing angles. 
+elev = torch.linspace(0, 180, batch_size)
+azim = torch.linspace(-180, 180, batch_size)
+
+# All the cameras helper methods support mixed type inputs and broadcasting. So we can 
+# view the camera from the same distance and specify dist=2.7 as a float,
+# and then specify elevation and azimuth angles for each viewpoint as tensors. 
+R, T = look_at_view_transform(dist=2.7, elev=elev, azim=azim)
+cameras = OpenGLPerspectiveCameras(device=device, R=R, T=T)
+
+# Move the light back in front of the cow which is facing the -z direction.
+lights.location = torch.tensor([[0.0, 0.0, -3.0]], device=device)
+
+
+# In[11]:
+
+
+# We can pass arbirary keyword arguments to the rasterizer/shader via the renderer
+# so the renderer does not need to be reinitialized if any of the settings change.
+images = renderer(meshes, cameras=cameras, lights=lights)
+
+
+# In[14]:
+
+
+image_grid(images.cpu().numpy(), rows=4, cols=5, rgb=True)
+
+
+# ## 7. Conclusion
+# In this tutorial we learnt how to **load** a textured mesh from an obj file, initialize a PyTorch3D datastructure called **Meshes**, set up an **Renderer** consisting of a **Rasterizer** and a **Shader**, and modify several components of the rendering pipeline. 
