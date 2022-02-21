1cbf80dab6611efa305345fd8047bf6e2e4cbe6a
Theo-Cheynel <34627497+Theo-Cheynel@users.noreply.github.com>
2022-02-21 11:21:52 -0800

    Added `matrix_to_axis_angle` to the exports of `transforms` (#1085)
    Summary:
    # Changelist
    - `matrix_to_axis_angle` was declared in `pytorch3d/transforms/rotation_conversions.py` but never exported from the `__init__` file.
    
    Pull Request resolved: https://github.com/facebookresearch/pytorch3d/pull/1085
    
    Reviewed By: patricklabatut
    
    Differential Revision: D34379935
    
    Pulled By: bottler
    
    fbshipit-source-id: 993c12a176630f91d0f107f298f458b2b35032e5

--- pytorch3d/transforms/__init__.py
+++ pytorch3d/transforms/__init__.py
@@ -9,6 +9,7 @@ from .rotation_conversions import (
     axis_angle_to_matrix,
     axis_angle_to_quaternion,
     euler_angles_to_matrix,
+    matrix_to_axis_angle,
     matrix_to_euler_angles,
     matrix_to_quaternion,
     matrix_to_rotation_6d,
