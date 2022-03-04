16d0aa82c1bbf3788571c651f5149f3c4e91a47a
dmitryvinn <dmitryvinn@gmail.com>
2022-03-04 01:51:16 -0800

    docs: add social banner in support of Ukraine (#1101)
    Summary:
    Our mission at [Meta Open Source](https://opensource.facebook.com/) is to empower communities through open source, and we believe that it means building a welcoming and safe environment for all. As a part of this work, we are adding this banner in support for Ukraine during this crisis.
    
    ![image](https://user-images.githubusercontent.com/12485205/156670302-756fac7c-51ba-4e24-b463-f79730dbaba6.png)
    
    Pull Request resolved: https://github.com/facebookresearch/pytorch3d/pull/1101
    
    Reviewed By: bottler
    
    Differential Revision: D34628257
    
    Pulled By: dmitryvinn-fb
    
    fbshipit-source-id: 5863afb59a2b9431e8e2ebc5856254ab0cdfcfe8

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -74,6 +74,20 @@ class HomeSplash extends React.Component {
   }
 }
 
+function SocialBanner() {
+  return (
+    <div className="socialBanner">
+      <div>
+        Support Ukraine 🇺🇦{' '}
+        <a href="https://opensource.fb.com/support-ukraine">
+          Help Provide Humanitarian Aid to Ukraine
+        </a>
+        .
+      </div>
+    </div>
+  );
+}
+
 class Index extends React.Component {
   render() {
     const {config: siteConfig, language = ''} = this.props;
@@ -212,6 +226,7 @@ loss_chamfer, _ = chamfer_distance(sample_sphere, sample_test)
 
     return (
       <div>
+        <SocialBanner />
         <HomeSplash siteConfig={siteConfig} language={language} />
         <div className="landingPage mainContainer">
           <Features />
