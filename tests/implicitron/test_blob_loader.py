44cfcfb9f243c16f6153617166eb28461705f1cc
Ildar Salakhiev <ildar@fb.com>
2023-03-09 13:28:54 +0000

    updating test_blob_loader WIP

--- tests/implicitron/test_blob_loader.py
+++ tests/implicitron/test_blob_loader.py
@@ -1,3 +1,5 @@
+import os
+import math
 import contextlib
 import unittest
 
@@ -14,6 +16,7 @@ from pytorch3d.implicitron.dataset.blob_loader import (
 )
 from pytorch3d.implicitron.dataset.json_index_dataset import JsonIndexDataset
 from pytorch3d.implicitron.dataset.blob_loader import BlobLoader
+from pytorch3d.renderer.cameras import PerspectiveCameras
 from tests.common_testing import TestCaseMixin
 from pytorch3d.implicitron.tools.config import expand_args_fields
 from pytorch3d.implicitron.tools.config import get_default_args
@@ -27,7 +30,7 @@ class TestBlobLoader(TestCaseMixin, unittest.TestCase):
 
         category = "skateboard"
         stack = contextlib.ExitStack()
-        dataset_root, path_manager = stack.enter_context(get_skateboard_data())
+        self.dataset_root, self.path_manager = stack.enter_context(get_skateboard_data())
         self.addCleanup(stack.close)
         frame_file = os.path.join(dataset_root, category, "frame_annotations.jgz")
         sequence_file = os.path.join(dataset_root, category, "sequence_annotations.jgz")
@@ -35,17 +38,18 @@ class TestBlobLoader(TestCaseMixin, unittest.TestCase):
 
         expand_args_fields(JsonIndexDataset)
 
-        self.datasets = JsonIndexDataset(
+        self.dataset = JsonIndexDataset(
                 frame_annotations_file=frame_file,
                 sequence_annotations_file=sequence_file,
-                dataset_root=dataset_root,
+                dataset_root=self.dataset_root,
                 image_height=self.image_size,
                 image_width=self.image_size,
                 box_crop=True,
                 load_point_clouds=True,
-                path_manager=path_manager,
+                path_manager=self.path_manager,
         )
-        self.entry = self.datasets.frame_annots[index]["frame_annotation"]
+        index = 7000
+        self.entry = self.dataset.frame_annots[index]["frame_annotation"]
 
     def test_BlobLoader_args(self):
         # test that BlobLoader works with get_default_args
@@ -58,57 +62,66 @@ class TestBlobLoader(TestCaseMixin, unittest.TestCase):
             bbox_xywh,
             clamp_bbox_xyxy,
             crop_bbox_xywh,
-        ) = self.datasets.loader._load_crop_fg_probability(entry)
+        ) = self.dataset.blob_loader._load_crop_fg_probability(self.entry)
 
-        assert fg_probability
+        assert torch.is_tensor(fg_probability)
         assert mask_path
         assert bbox_xywh
-        assert clamp_bbox_xyxy
-        assert crop_bbox_xywh
+        assert torch.is_tensor(clamp_bbox_xyxy)
+        assert torch.is_tensor(crop_bbox_xywh)
         (
             image_rgb,
             image_path,
             mask_crop,
             scale,
-        ) = self.dataset.loader._load_crop_images(
+        ) = self.dataset.blob_loader._load_crop_images(
             self.entry, fg_probability, clamp_bbox_xyxy,
         )
-        assert image_rgb
+        assert torch.is_tensor(image_rgb)
         assert image_path
-        assert mask_crop
+        assert torch.is_tensor(mask_crop)
         assert scale
         (
             depth_map,
             depth_path,
             depth_mask,
-        ) = self.dataset.loader._load_mask_depth(
+        ) = self.dataset.blob_loader._load_mask_depth(
             self.entry, clamp_bbox_xyxy, fg_probability,
         )
-        assert depth_map
-        assert depth_path
-        assert depth_mask
+        assert torch.is_tensor(depth_map)
+        assert torch.is_tensor(depth_path)
+        assert torch.is_tensor(depth_mask)
 
-        camera = self.dataset.loader._get_pytorch3d_camera(
+        camera = self.dataset.blob_loader._get_pytorch3d_camera(
                 self.entry, scale, clamp_bbox_xyxy,
             )
-        assert camera
+        assert type(camera) == PerspectiveCameras
 
     def test_fix_point_cloud_path(self):
         """Some files in Co3Dv2 have an accidental absolute path stored."""
         original_path = 'some_file_path'
-        modified_path = self.dataset.loader._fix_point_cloud_path(original_path)
+        modified_path = self.dataset.blob_loader._fix_point_cloud_path(original_path)
         assert original_path in modified_path
-        assert self.dataset.loader.dataset_root in modified_path
+        assert self.dataset.blob_loader.dataset_root in modified_path
 
     def test_resize_image(self):
-        image = None
-        image_rgb, scale, mask_crop = self.dataset.loader._resize_image(image)
-        assert image_rgb.shape == (self.dataset.loader.width, self.dataset.loader.height)
-        assert scale == 1
-        assert masc_crop.shape == (self.dataset.loader.width, self.dataset.loader.height)
+        path = os.path.join(self.dataset_root, self.entry.image.path)
+        local_path = self.path_manager.get_local_path(path)
+        image = _load_image(local_path)
+        image_rgb, scale, mask_crop = self.dataset.blob_loader._resize_image(image)
+
+        original_shape = image.shape[-2:]
+        expected_shape = (self.dataset.blob_loader.image_width, self.dataset.blob_loader.image_height)
+        expected_scale = expected_shape[0] / original_shape[0]
+
+        assert scale == expected_scale
+        assert image_rgb.shape[-2:] == expected_shape
+        assert mask_crop.shape[-2:] == expected_shape
 
     def test_load_image(self):
-        image = _load_image(self.entry.image.path)
+        path = os.path.join(self.dataset_root, self.entry.image.path)
+        local_path = self.path_manager.get_local_path(path)
+        image = _load_image(local_path)
         assert image.dtype == np.float32
         assert torch.max(image) <= 1.0
         assert torch.min(image) >= 0.0
@@ -120,32 +133,27 @@ class TestBlobLoader(TestCaseMixin, unittest.TestCase):
         assert torch.min(mask) >= 0.0
 
     def test_load_depth(self):
-        entry_depth = self.entry.depth
-        # path = os.path.join(self.dataset_root, entry_depth.path)
+        path = os.path.join(self.dataset_root, entry_depth.path)
         path = entry_depth.path
         depth_map = _load_depth(path, entry_depth.scale_adjustment)
         assert depth_map.dtype == np.float32
         assert depth_map.shape
 
     def test_load_16big_png_depth(self):
-        entry_depth = self.entry.depth
-        # path = os.path.join(self.dataset_root, entry_depth.path)
-        path = entry_depth.path
+        path = os.path.join(self.dataset_root, self.entry.depth.path)
         depth_map = _load_16big_png_depth(path)
         assert depth_map.dtype == np.float32
         assert depth_map.shape
 
     def test_load_1bit_png_mask(self):
-        entry_depth = self.entry.depth
-        # mask_path = os.path.join(self.dataset_root, entry_depth.mask_path)
+        mask_path = os.path.join(self.dataset_root, self.entry.depth.mask_path)
         mask_path = entry_depth.mask_path
         mask = _load_16big_png_depth(mask_path)
         assert mask.dtype == np.float32
         assert mask.shape
 
     def test_load_depth_mask(self):
-        entry_depth = self.entry.depth
-        # mask_path = os.path.join(self.dataset_root, entry_depth.mask_path)
+        mask_path = os.path.join(self.dataset_root, self.entry.depth.mask_path)
         mask_path = entry_depth.mask_path
         mask = _load_depth_mask(mask_path)
         assert mask.dtype == np.float32
