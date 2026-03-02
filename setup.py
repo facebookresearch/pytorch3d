a931ab2763900175590411ab54b3364dfbfe5e2d
artabish <abdul.tabish@quidient.com>
2026-03-02 18:17:19 -0500

    Revert "Test commit for branch protection"
    This reverts commit e78f35f584af7ddc5ecb2064845acba3b45b0b2f.

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # Copyright (c) Meta Platforms, Inc. and affiliates.
-# Test commit for branch protection
 # All rights reserved.
 #
 # This source code is licensed under the BSD-style license found in the
